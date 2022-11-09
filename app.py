40d63b6ede63f1b44e6802d21bcda111f1d157b7
kuda2012 <kuda2012@gmail.com>
2022-11-09 17:12:59 -0600

    update pull

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ CURR_USER_KEY = "curr_user"
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 test_db = None
-# test_db = "postgres://postgres@127.0.0.1:5432/book_db_test"
+test_db = "postgres://postgres@127.0.0.1:5432/book_db_test"
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', test_db or "postgres://postgres@127.0.0.1:5432/book_db"))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
